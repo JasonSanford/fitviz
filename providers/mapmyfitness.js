7a8a99b224b1eecd604451f69936520a83fd5a3d
Jason Sanford <jasonsanford@gmail.com>
2015-01-03 07:06:54 -0500

    parse mmf workout

--- providers/mapmyfitness.js
+++ providers/mapmyfitness.js
@@ -19,11 +19,32 @@ function mmfApiRequest (path, params, accessToken, callback) {
 }
 
 function getWorkouts (user, pageInfo, callback) {
-  mmfApiRequest('workout', { order_by: '-start_datetime', user: user.provider_id }, user.access_token, function (error, response, body) {
+  var limit = pageInfo.perPage;
+  var offset = pageInfo.page === 1 ? 0 : ((pageInfo.page - 1) * pageInfo.perPage);
+  var params = {
+    order_by: '-start_datetime',
+    user: user.provider_id,
+    limit: limit,
+    offset: offset
+  };
+  mmfApiRequest('workout', params, user.access_token, function (error, response, body) {
     if (error) {
       callback(error);
     } else {
-      callback(null, body);
+      var mmfWorkouts = body._embedded.workouts;
+      var workouts = mmfWorkouts.map(function (mmfWorkout) {
+        return {
+          start_date      : mmfWorkout.start_datetime,
+          name            : mmfWorkout.name,
+          id              : mmfWorkout._links.self[0].id,
+          has_time_series : mmfWorkout.has_time_series,
+          active_time     : mmfWorkout.aggregates.active_time_total,
+          distance        : mmfWorkout.aggregates.distance_total,
+          heart_rate_avg  : mmfWorkout.aggregates.heartrate_avg,
+          heart_rate_max  : mmfWorkout.aggregates.heartrate_max
+        };
+      });
+      callback(null, workouts);
     }
   });
 }
