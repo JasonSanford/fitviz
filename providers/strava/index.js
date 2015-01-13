3cf012ca8aee93d83e1e929e972c5c4f3f90752f
Jason Sanford <jasonsanford@gmail.com>
2015-01-12 23:57:35 -0500

    Strava viz mostly working.

--- providers/strava/index.js
+++ providers/strava/index.js
@@ -1,7 +1,88 @@
 var strava = require('strava-v3');
 
+var metrics = require('../../metrics');
+
+var normalizedMetrics = {
+  watts    : 'power',
+  altitude : 'elevation'
+};
+
 function getWorkout (user, workoutId, callback) {
-  
+  var params = {
+    access_token : user.access_token,
+    id           : workoutId,
+    types        : 'latlng,altitude,heartrate,cadence,watts'
+  };
+  strava.streams.activity(params, function (error, streams) {
+    if (error) {
+      return callback(error);
+    }
+
+    var i, coordinates, coordinatesStream;
+    var availableMetrics = [];
+    var aggregates       = {};
+    var ignoreMetrics    = ['distance'];
+
+    for (i = 0; i < streams.length; i++) {
+      stream = streams[i];
+      if (stream.type === 'latlng') {
+        coordinatesStream = streams[i];
+        break;
+      }
+    }
+
+    coordinates = coordinatesStream.data.map(function (latLng) {
+      return [latLng[1], latLng[0]];
+    });
+
+    streams.forEach(function (stream) {
+      var normalizedMetricKey;
+      if (stream.type !== 'latlng') {  // We already handled latlng
+        if (stream.type in metrics || stream.type in normalizedMetrics) {
+          if (stream.type in metrics) {
+            normalizedMetricKey = stream.type;
+          } else {
+            normalizedMetricKey = normalizedMetrics[stream.type];
+          }
+          if (ignoreMetrics.indexOf(normalizedMetricKey) < 0) {
+            availableMetrics.push(normalizedMetricKey);
+          }
+          stream.data.forEach(function (streamValue, index) {
+            coordinates[index][metrics[normalizedMetricKey].arrayPosition] = streamValue;
+            if (index === 0) {
+              aggregates[normalizedMetricKey + '_min'] = streamValue;
+              aggregates[normalizedMetricKey + '_max'] = streamValue;
+            } else {
+              if (streamValue < aggregates[normalizedMetricKey + '_min']) {
+                aggregates[normalizedMetricKey + '_min'] = streamValue;
+              }
+              if (streamValue > aggregates[normalizedMetricKey + '_max']) {
+                aggregates[normalizedMetricKey + '_max'] = streamValue;
+              }
+            }
+          });
+        } else {
+          console.log('No metric found: ' + stream.type + '. Skipping.');
+        }
+      }
+    });
+
+    var feature = {
+      id         : workoutId,
+      type       : 'Feature',
+      properties : {
+        available_metrics : availableMetrics,
+        metrics           : metrics,
+        aggregates        : aggregates
+      },
+      geometry   : {
+        type        : 'LineString',
+        coordinates : coordinates
+      }
+    };
+
+    callback(null, feature);
+  });
 }
 
 function getWorkouts (user, pageInfo, callback) {
--- providers/underarmour/index.js
+++ providers/underarmour/index.js
@@ -2,44 +2,7 @@ var request = require('request');
 
 var activityTypes = require('./activity_types');
 var constants     = require('../../constants');
-
-var metrics = {
-  'lng': {
-    arrayPosition: 0,
-    display: 'Longitude'
-  },
-  'lat': {
-    arrayPosition: 1,
-    display: 'Latitude'
-  },
-  'elevation': {
-    arrayPosition: 2,
-    display: 'Elevation'
-  },
-  'distance': {
-    arrayPosition: 3,
-    display: 'Distance'
-  },
-  'heartrate': {
-    arrayPosition: 4,
-    display: 'Heart Rate',
-    unit: 'bpm'
-  },
-  'speed': {
-    arrayPosition: 5,
-    display: 'Speed'
-  },
-  'cadence': {
-    arrayPosition: 6,
-    display: 'Cadence',
-    unit: 'rpm'
-  },
-  'power': {
-    arrayPosition: 7,
-    display: 'Power',
-    unit: 'watts'
-  }
-};
+var metrics       = require('../../metrics');
 
 function uaApiRequest (path, params, accessToken, callback) {
   var apiBase = 'https://api.ua.com/v7.0/';
