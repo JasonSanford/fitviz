3535fc20c352de61ef68a6134055bc0e4946b5ca
Jason Sanford <jasonsanford@gmail.com>
2015-01-13 10:27:43 -0500

    Update map style.

--- public/js/constants.js
+++ public/js/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
   mapboxAccessToken : 'pk.eyJ1IjoiamNzYW5mb3JkIiwiYSI6InRJMHZPZFUifQ.F4DMGoNgU3r2AWLY0Eni-w',
-  mapboxMapId       : 'jcsanford.kmdnbkib',
+  mapboxMapId       : 'jcsanford.1c312c69',
   metersPerMile     : 1609.34
 };
